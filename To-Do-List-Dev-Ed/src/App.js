0541e835fa46107a17da3f62a715bbb895a9ba16
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-06-30 17:59:22 +0300

    todolistapp

--- To-Do-List-Dev-Ed/src/App.js
+++ To-Do-List-Dev-Ed/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from './Form';
+import TodoList from './TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header>
+           <h1>Ionut Todo List</h1>
+        </header>
+           <Form 
+           inputText={inputText} 
+           todos={todos} 
+           setTodos={setTodos} 
+           setInputText={setInputText}
+           />
+           <TodoList setTodos={setTodos} todos={todos}/>
+      </div>
   );
 }
 
